6f9e58e5d6d7e472fe480e9bad92cd7a14a0c20f
Neesh <avaneeshsamsi@gmail.com>
2021-10-22 16:21:09 +0530

    working template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center flex-col">
-      <img
-        src="https://res.cloudinary.com/practicaldev/image/fetch/s--JXlFx1ne--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/f30hrij5h3j7gt2klbcu.jpg"
-        alt="Tailwind Logo"
-        className="w-1/4"
-      />
-      <h1 className="text-gray-900 text-5xl">CRA Tailwind Template 🔥</h1>
-      <small className="my-3 text-gray-600 text-md absolute bottom-10">
-        Made with 💙 by{" "}
-        <a
-          href="https://www.github.com/neeshsamsi"
-          target="_blank"
-          rel="noreferrer"
-          className="text-green-600 underline"
-        >
-          Neesh Samsi
-        </a>
-      </small>
+    <div className="flex justify-center">
+      <h1 className="text-green-700 text-5xl">Hello Tailwind!</h1>
     </div>
   );
 }
